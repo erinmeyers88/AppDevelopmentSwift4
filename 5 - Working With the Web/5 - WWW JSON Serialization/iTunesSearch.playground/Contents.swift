882850db278fcb11d1c5fa6cbe79ceec81727f67
Vincent, Erin <evincent@ariit.com>
2019-08-05 10:59:12 -0600

    iTunesSearch

---
+++ 5 - Working With the Web/5 - WWW JSON Serialization/iTunesSearch.playground/Contents.swift
@@ -0,0 +1,32 @@
+import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+extension URL {
+    func withQueries(_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self,
+                                       resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.flatMap
+            { URLQueryItem(name: $0.0, value: $0.1) }
+        return components?.url
+    }
+}
+let query: [String: String] = [
+    "term": "Kelly Clarkson",
+    "media": "music"
+]
+
+let baseUrl = URL(string: "https://itunes.apple.com/search")!
+
+let url = baseUrl.withQueries(query)!
+
+let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+    if let data = data, let string = String(data: data, encoding: .utf8) {
+        print(string)
+    }
+}
+
+task.resume()
+
+PlaygroundPage.current.finishExecution()
