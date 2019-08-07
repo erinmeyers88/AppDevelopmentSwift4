ded7258a59fa0111dc1ec7835b56fbbeaa7d1ada
Vincent, Erin <evincent@ariit.com>
2019-08-07 10:15:41 -0600

    added missing projects

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,44 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Erin Vincent on 1/23/18.
+//  Copyright © 2018 Erin Vincent. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button Action
+    
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed")
+    }
+    
+    //MARK: Private Methods
+    
+    private func setupButtons() {
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+        addArrangedSubview(button)
+    }
+}
---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Erin Vincent on 1/22/18.
+//  Copyright © 2018 Erin Vincent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    //MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        else {
+            fatalError("Expected a dictionary containg an image, but provided the following: \(info)")
+        }
+        
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: Actions
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
+}
+
